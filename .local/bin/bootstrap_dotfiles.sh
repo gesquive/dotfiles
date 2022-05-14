801b23aeb6d3f7a7dafe6028b8ffd307c1ebb13d
Gus Esquivel <gesquive@gmail.com>
2022-05-13 23:13:58 -0500

    fix bootstrap yadm dl
    fix bootstrap yadm dl

--- .local/bin/bootstrap_dotfiles.sh
+++ .local/bin/bootstrap_dotfiles.sh
@@ -7,8 +7,8 @@ YADM=${HOME}/.local/bin/yadm
 mkdir -p ./local/bin
 
 # Download yadm
-curl -fLo ${YADM} https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/local/bin/yadm
+curl -fLo ${YADM} https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x ${YADM}
 
 # have yadm grab the dotfiles and bootstrap
-${YADM} clone --bootstrap git@github.com:gesquive/dotfiles.git
+${YADM} clone -f --bootstrap git@github.com:gesquive/dotfiles.git
 ${YADM} status
