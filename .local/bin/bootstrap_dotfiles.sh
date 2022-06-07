64abe775e35f5f08d2a62a97b299cea4fdb87867
Gus Esquivel <gesquive@gmail.com>
2022-06-06 21:32:49 -0500

    add color support and allow for alternate repos

--- .local/bin/bootstrap_dotfiles.sh
+++ .local/bin/bootstrap_dotfiles.sh
@@ -1,14 +1,59 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 YADM=${HOME}/.local/bin/yadm
 
-mkdir -p ./local/bin
+BOLD="$(tput bold 2>/dev/null || printf '')"
+GREY="$(tput setaf 0 2>/dev/null || printf '')"
+UNDERLINE="$(tput smul 2>/dev/null || printf '')"
+RED="$(tput setaf 1 2>/dev/null || printf '')"
+GREEN="$(tput setaf 2 2>/dev/null || printf '')"
+YELLOW="$(tput setaf 3 2>/dev/null || printf '')"
+BLUE="$(tput setaf 4 2>/dev/null || printf '')"
+MAGENTA="$(tput setaf 5 2>/dev/null || printf '')"
+NO_COLOR="$(tput sgr0 2>/dev/null || printf '')"
 
-# Download yadm
-curl -fLo ${YADM} https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x ${YADM}
+
+function info() {
+  printf '%s\n' "${BOLD}${GREY}>${NO_COLOR} $*"
+}
+
+function warn() {
+  printf '%s\n' "${YELLOW}! $*${NO_COLOR}"
+}
+
+function error() {
+  printf '%s\n' "${RED}x $*${NO_COLOR}" >&2
+}
+
+functioncompleted() {
+  printf '%s\n' "${GREEN}✓${NO_COLOR} $*"
+}
+
+has() {
+  command -v "$1" 1>/dev/null 2>&1
+}
+
+if ! has git; then
+    error "git is not installed"
+    exit 1
+fi
+
+if ! has yadm; then # install yadm
+    mkdir -p $(dirname ${YADM})
+    curl -fLo ${YADM} https://github.com/TheLocehiliosan/yadm/raw/master/yadm
+    chmod a+x ${YADM}
+fi
+
+info "repo: ${REPO}"
+if [ -z "${REPO}" ]; then
+    REPO=https://github.com/gesquive/dotfiles.git}
+    if ssh -q git@github.com; [ $? -eq 1 ]; then
+        REPO=git@github.com:gesquive/dotfiles.git
+    fi
+fi
 
 # have yadm grab the dotfiles and bootstrap
-${YADM} clone -f --bootstrap git@github.com:gesquive/dotfiles.git
+${YADM} clone -f --bootstrap ${REPO}
 ${YADM} status
